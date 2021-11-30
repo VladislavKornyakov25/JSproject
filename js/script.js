16a98183ae33bc16ce318f0ba6659bf0e4964a90
Vlad <example@gmail.com>
2021-11-30 20:30:13 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = prompt("Are you 18?", "" );
+//console.log(answer + 5);
+
+//const answers = [];
+
+// answers[0] = prompt("What's your name?", "");
+// answers[1] = prompt("What's your last name?", "");
+// answers[2] = prompt("How old are you?", "");
+
+//console.log(typeof(answers));
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Iavn';
+
+// alert(`Hello, ${user}`);
+
+let incr = 10,
+    decr = 10;
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
+
+console.log(6/2*(1+2));
